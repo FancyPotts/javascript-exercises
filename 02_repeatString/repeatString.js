68a9454ccc6a2ed43b0a87d621a2b33f5a06b5a1
pottsaf <aaronfpotts@gmail.com>
2023-03-18 14:40:41 -0600

    Complete 2nd exercise and pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR"
+    } else {
+        let repeat = '';
+        while (num > 0) {
+            repeat += string;
+            num--;
+        }
+            return repeat;
+    }
 };
 
 // Do not edit below this line
